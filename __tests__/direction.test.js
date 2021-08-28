0d825e0bb526d0333e95d91c254b8205531cb5c0
TDC3Tool <tdc3contact@gmail.com>
2021-08-28 13:57:32 -0300

    Updating test description

--- __tests__/direction.test.js
+++ __tests__/direction.test.js
@@ -2,7 +2,7 @@ const Direction = require("../src/direction.js");
 
 describe("Direction", () => {
   const direction = new Direction("N");
-  it("initiates Direction", () => {
+  it("checks if the value of direction is N", () => {
     expect(direction.value).toBe("N");
   });
 });
